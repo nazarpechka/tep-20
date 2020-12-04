26e7736646b47e71bfa723cadefc69a7d9f3f5c0
Nazar Pechevystyi <nazarpechka@gmail.com>
2020-12-04 14:06:51 +0100

    Use templates for dynamic tree

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 
 #include "tree_dynamic.h"
-#include "tree_static.h"
-
-#define CURR_TREE TreeStatic
 
 void TestTree() {
-  std::cout << "Example tree:\n";
+  std::cout << "Example trees:\n";
   /*
             0
         1      2
       11 12  21 22
   */
-  CURR_TREE tree;
+  TreeDynamic<int> tree;
 
   tree.GetRoot()->AddChild();
   tree.GetRoot()->AddChild();
@@ -32,70 +29,22 @@ void TestTree() {
   tree.GetRoot()->GetChild(1)->GetChild(0)->SetValue(21);
   tree.GetRoot()->GetChild(1)->GetChild(1)->SetValue(22);
 
-  std::cout << tree.GetRoot()->GetChild(0)->FromTheSameTree(
-                   tree.GetRoot()->GetChild(1))
-            << '\n';
-
   tree.PrintTree();
+  std::cout << '\n';
+
+  TreeDynamic<std::string> second_tree;
+
+  second_tree.GetRoot()->AddChild();
+  second_tree.GetRoot()->AddChild();
+
+  second_tree.GetRoot()->GetChild(0)->SetValue("Hello");
+  second_tree.GetRoot()->GetChild(1)->SetValue("World");
+
+  second_tree.GetRoot()->GetChild(0)->AddChild();
+  second_tree.GetRoot()->GetChild(0)->GetChild(0)->SetValue("123");
+
+  second_tree.PrintTree();
   std::cout << '\n';
 }
 
-void TestMoving() {
-  std::cout << "Before moving: \n";
-  CURR_TREE first_tree;
-
-  first_tree.GetRoot()->AddChild();
-  first_tree.GetRoot()->AddChild();
-  first_tree.GetRoot()->AddChild();
-
-  first_tree.GetRoot()->GetChild(0)->SetValue(1);
-  first_tree.GetRoot()->GetChild(1)->SetValue(2);
-  first_tree.GetRoot()->GetChild(2)->SetValue(3);
-
-  first_tree.GetRoot()->GetChild(2)->AddChild();
-  first_tree.GetRoot()->GetChild(2)->GetChild(0)->SetValue(4);
-
-  first_tree.PrintTree();
-
-  CURR_TREE second_tree;
-
-  second_tree.GetRoot()->SetValue(50);
-
-  second_tree.GetRoot()->AddChild();
-  second_tree.GetRoot()->AddChild();
-
-  second_tree.GetRoot()->GetChild(0)->SetValue(54);
-  second_tree.GetRoot()->GetChild(1)->SetValue(55);
-
-  second_tree.GetRoot()->GetChild(0)->AddChild();
-  second_tree.GetRoot()->GetChild(0)->AddChild();
-
-  second_tree.GetRoot()->GetChild(0)->GetChild(0)->SetValue(56);
-  second_tree.GetRoot()->GetChild(0)->GetChild(1)->SetValue(57);
-
-  second_tree.GetRoot()->GetChild(0)->GetChild(0)->AddChild();
-
-  second_tree.GetRoot()->GetChild(0)->GetChild(0)->GetChild(0)->SetValue(58);
-
-  second_tree.PrintTree();
-
-  std::cout << first_tree.GetRoot()->GetChild(0)->FromTheSameTree(
-                   second_tree.GetRoot()->GetChild(0)->GetChild(0))
-            << '\n';
-
-  // first_tree.GetRoot()->GetChild(2)->MoveSubtree(
-  //     second_tree.GetRoot()->GetChild(0));
-
-  // std::cout << "After moving:\n";
-  // first_tree.PrintTree();
-  // second_tree.PrintTree();
-
-  // std::cout << "From bottom to top:\n";
-  // first_tree.GetRoot()->GetChild(2)->GetChild(1)->GetChild(1)->PrintUp();
-  // second_tree.GetRoot()->GetChild(0)->PrintUp();
-}
-
-int main(void) {
-  TestTree();
-  TestMoving();
-}
+int main(void) { TestTree(); }
--- src/tree_dynamic.h
+++ src/tree_dynamic.h
@@ -3,41 +3,183 @@
 
 #include <vector>
 
+template <typename T>
 class NodeDynamic {
  public:
   NodeDynamic();
   ~NodeDynamic();
 
-  void SetValue(int new_val);
-  int GetChildrenNumber() const;
-  void SetParent(NodeDynamic *parent);
-  NodeDynamic *GetParent() const;
+  void SetValue(T new_val);
+  int GetChildrenCount() const;
+  void SetParent(NodeDynamic<T>* parent);
+  NodeDynamic<T>* GetParent() const;
   void AddChild();
-  NodeDynamic *GetChild(unsigned int offset);
+  NodeDynamic<T>* GetChild(unsigned int offset);
 
   void UpdateChildrenPointers();
-  bool MoveSubtree(NodeDynamic *child);
-  bool FromTheSameTree(NodeDynamic *second);
+  bool MoveSubtree(NodeDynamic<T>* child);
+  bool FromTheSameTree(NodeDynamic<T>* other);
   void Print() const;
   void PrintUp() const;
   void PrintDown() const;
 
  private:
-  std::vector<NodeDynamic *> children_;
-  NodeDynamic *parent_;
-  int val_;
+  std::vector<NodeDynamic<T>*> children_;
+  NodeDynamic<T>* parent_;
+  T val_;
 };
 
+template <typename T>
 class TreeDynamic {
  public:
   TreeDynamic();
   ~TreeDynamic();
 
-  NodeDynamic *GetRoot();
+  NodeDynamic<T>* GetRoot();
   void PrintTree();
 
  private:
-  NodeDynamic *root;
+  NodeDynamic<T>* root;
 };
+
+template <typename T>
+NodeDynamic<T>::NodeDynamic() {
+  parent_ = NULL;
+  children_.reserve(3);
+}
+
+template <typename T>
+NodeDynamic<T>::~NodeDynamic() {
+  int size = children_.size();
+  for (int i = 0; i < size; ++i) {
+    delete children_[i];
+  }
+}
+
+template <typename T>
+void NodeDynamic<T>::SetValue(T new_val) {
+  val_ = new_val;
+}
+
+template <typename T>
+int NodeDynamic<T>::GetChildrenCount() const {
+  return children_.size();
+}
+
+template <typename T>
+void NodeDynamic<T>::SetParent(NodeDynamic<T>* parent) {
+  parent_ = parent;
+}
+
+template <typename T>
+NodeDynamic<T>* NodeDynamic<T>::GetParent() const {
+  return parent_;
+}
+
+template <typename T>
+void NodeDynamic<T>::AddChild() {
+  NodeDynamic<T>* child = new NodeDynamic<T>();
+  child->SetParent(this);
+  children_.push_back(child);
+}
+
+template <typename T>
+NodeDynamic<T>* NodeDynamic<T>::GetChild(unsigned int offset) {
+  return (offset < children_.size() ? children_[offset] : NULL);
+}
+
+template <typename T>
+void NodeDynamic<T>::UpdateChildrenPointers() {
+  const int size = children_.size();
+  for (int i = 0; i < size; ++i) {
+    children_[i]->SetParent(this);
+  }
+}
+
+template <typename T>
+bool NodeDynamic<T>::MoveSubtree(NodeDynamic<T>* child) {
+  if (child == NULL) return false;
+
+  if (child->parent_ != NULL) {
+    // Remove the child from previous parent
+    typename std::vector<NodeDynamic<T>*>& parent_vector =
+        child->parent_->children_;
+    typename std::vector<NodeDynamic<T>*>::iterator iter =
+        std::find(parent_vector.begin(), parent_vector.end(), child);
+
+    if (iter != parent_vector.end()) parent_vector.erase(iter);
+  } else {
+    // If child is root in the other tree, create a copy
+    NodeDynamic<T>* child_copy = new NodeDynamic<T>(*child);
+    child->children_.clear();
+    child->val_ = 0;
+    child = child_copy;
+    child->UpdateChildrenPointers();
+  }
+
+  child->SetParent(this);
+  children_.push_back(child);
+
+  return true;
+}
+
+template <typename T>
+bool NodeDynamic<T>::FromTheSameTree(NodeDynamic<T>* other) {
+  NodeDynamic<T>* root_first = this;
+  NodeDynamic<T>* root_second = other;
+
+  while (root_first->parent_ != NULL) {
+    root_first = root_first->parent_;
+  }
+
+  while (root_second->parent_ != NULL) {
+    root_second = root_second->parent_;
+  }
+
+  return root_first == root_second;
+}
+
+template <typename T>
+void NodeDynamic<T>::Print() const {
+  std::cout << " " << val_;
+}
+
+template <typename T>
+void NodeDynamic<T>::PrintUp() const {
+  Print();
+  if (parent_ != NULL)
+    parent_->PrintUp();
+  else
+    std::cout << '\n';
+}
+
+template <typename T>
+void NodeDynamic<T>::PrintDown() const {
+  Print();
+  for (size_t i = 0; i < children_.size(); ++i) {
+    children_[i]->PrintDown();
+  }
+}
+
+template <typename T>
+TreeDynamic<T>::TreeDynamic() {
+  root = new NodeDynamic<T>();
+}
+
+template <typename T>
+TreeDynamic<T>::~TreeDynamic() {
+  delete root;
+}
+
+template <typename T>
+NodeDynamic<T>* TreeDynamic<T>::GetRoot() {
+  return root;
+}
+
+template <typename T>
+void TreeDynamic<T>::PrintTree() {
+  root->PrintDown();
+  std::cout << '\n';
+}
 
 #endif
